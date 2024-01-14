935309fc319f9e0c6e08c55bb0414dcc59b90f0d
Andrei <sucmanandrei@gmail.com>
2024-01-14 09:07:52 +0300

    tweaks

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useRef } from "react";
+import { VscEmptyWindow } from "react-icons/vsc";
 import Head from "next/head";
+import EmptyPlaceholder from "@/components/EmptyPlaceholder";
 import { getDocuments } from "outstatic/server";
 import BlogCard from "@/components/BlogCard";
 import styles from "./Blog.module.scss";
@@ -25,23 +27,31 @@ export default function Index({ posts }: Props) {
       </Head>
       <div className={styles.container}>
         <h2>Blog</h2>
-        <div className={styles.content}>
-          {posts.map((post: any, index: number) => {
-            if (index > showUntil) return;
-            return (
-              <React.Fragment key={index}>
-                <BlogCard
-                  title={post.title}
-                  imageUrl={post.coverImage}
-                  description={post.description}
-                  date={post.date}
-                  author={post.author}
-                  slug={post.slug}
-                />
-              </React.Fragment>
-            );
-          })}
-        </div>
+        {posts.length > 0 ? (
+          <div className={styles.content}>
+            {posts.map((post: any, index: number) => {
+              if (index > showUntil) return;
+              return (
+                <React.Fragment key={index}>
+                  <BlogCard
+                    title={post.title}
+                    imageUrl={post.coverImage}
+                    description={post.description}
+                    date={post.date}
+                    author={post.author}
+                    slug={post.slug}
+                  />
+                </React.Fragment>
+              );
+            })}
+          </div>
+        ) : (
+          <EmptyPlaceholder
+            icon={<VscEmptyWindow style={{ width: "2rem", height: "2rem" }} />}
+            message="No content"
+            customStyle={{ backgroundColor: "white" }}
+          />
+        )}
         {hasMoreRef.current && (
           <button
             className={styles.more}
