9441f44f0175e4bff73630d6d698788e828ab795
Andrei <sucmanandrei@gmail.com>
2024-01-23 10:34:39 +0300

    test gi

--- pages/server-sitemap.xml.tsx
+++ pages/server-sitemap.xml.tsx
@@ -2,21 +2,25 @@ import { getDocuments } from "outstatic/server";
 import { getServerSideSitemapLegacy, ISitemapField } from "next-sitemap";
 
 export async function getServerSideProps(ctx: any) {
-  const posts = getDocuments("posts", [
-    "title",
-    "content",
-    "author",
-    "coverImage",
-    "description",
-    "slug",
-  ]);
+  // const posts = getDocuments("posts", [
+  //   "title",
+  //   "content",
+  //   "author",
+  //   "coverImage",
+  //   "description",
+  //   "slug",
+  // ]);
 
-  const fields: ISitemapField[] = posts.map((post) => ({
-    loc: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/blog/${post.slug}`,
-    lastmod: new Date().toISOString(),
-  }));
+  // const fields: ISitemapField[] = posts.map((post) => ({
+  //   loc: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/blog/${post.slug}`,
+  //   lastmod: new Date().toISOString(),
+  // }));
 
   const staticPages = [
+    {
+      loc: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/blog/how-ela-teachers-can-embrace-ai-essay-grading`,
+      lastmod: new Date().toISOString(),
+    },
     {
       loc: `${process.env.NEXT_PUBLIC_FRONTEND_URL}/blog`,
       lastmod: new Date().toISOString(),
@@ -39,9 +43,9 @@ export async function getServerSideProps(ctx: any) {
     },
   ];
 
-  fields.push(...staticPages);
+  // fields.push(...staticPages);
 
-  return getServerSideSitemapLegacy(ctx, fields);
+  return getServerSideSitemapLegacy(ctx, staticPages);
 }
 
 function SiteMap() {
