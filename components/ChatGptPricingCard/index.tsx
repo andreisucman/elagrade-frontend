8b5018a555eb39dcbdf162fe172e682d75f01f3e
Andrei <sucmanandrei@gmail.com>
2024-01-31 00:33:47 +0300

    test

--- components/ChatGptPricingCard/index.tsx
+++ components/ChatGptPricingCard/index.tsx
@@ -22,7 +22,11 @@ const ChatGptPricingCard = () => {
           <div className={styles.container}>
             <div className={styles.wrapper}>
               <p className={styles.about}>
-                If you have ChatGPT <b>Plus</b> plan you can use Elagrade free.
+                If you have ChatGPT <b>Plus</b> plan you can use the Elagrade App free.
+              </p>
+              <p className={styles.about_desc}>
+                (ChatGPT Plus is a $20/Month subscription service offered by
+                OpenAi)
               </p>
               <ul className={styles.list}>
                 <li className={styles.item}>
