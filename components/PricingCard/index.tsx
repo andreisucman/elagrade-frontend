55cb54699e3c93e76e36e4db8604230f6edecc94
Andrei <sucmanandrei@gmail.com>
2024-02-16 21:59:32 +0300

    test

--- components/PricingCard/index.tsx
+++ components/PricingCard/index.tsx
@@ -71,7 +71,7 @@ const PricingCard = ({
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.totalPrice}>${priceRef.current.toFixed(2)} USD</p>
         <p className={styles.totalPages}>
-          Pages: {pagesRef.current.toFixed(0)} (500 words)
+          Pages: {pagesRef.current.toFixed(0)} (500 words each)
         </p>
         <p>PPP: ${PPP} USD</p>
         <p className={styles.extra}>
