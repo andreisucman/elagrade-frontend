9a673cd60b73c24f58f85546809bf3f719005240
Andrei <sucmanandrei@gmail.com>
2024-09-09 11:29:14 +0300

    progress

--- next.config.js
+++ next.config.js
@@ -16,12 +16,6 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/**",
-      },
     ],
   },
 };
